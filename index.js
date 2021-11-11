bec916a60c9897e34fadaca3c5e8f5eb873036c7
Hayder K. Alhamadi <haidarfmg@gmail.com>
2021-11-11 17:09:27 +0300

    add password strength checker using regular expression

--- index.js
+++ index.js
@@ -21,3 +21,53 @@ btn.addEventListener('click', function() {
     }
 
 })
+
+
+
+//////////////////////////////////////////////////////////
+console.log("script loaded");
+
+var password = document.getElementById("password");
+var passwordValid = document.getElementById("password-vaild");
+var parentpasswordValid = document.getElementById("parent-password-vaild");
+
+
+password.addEventListener('input', function(e) {
+
+    parentpasswordValid.style.display = 'block';
+    value = e.target.value;
+
+    pattern = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/;
+    var vvv = pattern.test(value);
+    pattern2 = /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})/;
+    var vvv2 = pattern2.test(value);
+
+    if (vvv) {
+        passwordValid.classList.remove("bg-info");
+        passwordValid.classList.remove("bg-danger");
+        passwordValid.style.width = "100%";
+        passwordValid.classList.add("bg-success");
+        passwordValid.innerHTML = "100%";
+
+
+
+
+    } else {
+
+        if (vvv2) {
+            passwordValid.classList.remove("bg-success");
+            passwordValid.classList.remove("bg-danger");
+            passwordValid.style.width = "75%";
+            passwordValid.classList.add("bg-info");
+            passwordValid.innerHTML = "75%";
+
+        } else {
+            passwordValid.classList.remove("bg-info");
+            passwordValid.classList.remove("bg-success");
+            passwordValid.style.width = "25%";
+            passwordValid.classList.add("bg-danger");
+            passwordValid.innerHTML = "25%";
+        }
+
+    }
+});
